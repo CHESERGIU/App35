75652a81bacd21cad2f4ab2325c00963055f7495
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:45:45 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace App35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] numbers = text.Split(" ");
+            string reverse = "";
+
+            for (int i = numbers.Length - 1; i > -1; i--)
+            {                
+                reverse += numbers[i] + " ";
+            }
+            Console.WriteLine(reverse);
+        }
+    }
+}
